e58dc1009ec6f55b6619b1d2d4d0faa113efca69
mivan08 <mykhailoivanov662@gmail.com>
2022-09-22 23:40:32 +0300

    Deploy on Deno

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const connectDB = require('../config/db')
-const usersRoute = require('./routes/api/users')
 
 const app = express()
 
@@ -13,8 +12,9 @@ app.use(express.json({ extended: false }))
 app.get('/', (req, res) => res.send('API Running'))
 
 // Define Routes
-app.use('/api/users', usersRoute)
+app.use('/api/users', require('./routes/api/users'))
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
