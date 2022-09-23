c42a851cd2f3c10541c04d1055437d6365f4c58e
mivan08 <mykhailoivanov662@gmail.com>
2022-09-23 13:39:06 +0300

    Auth implemented

--- src/server.js
+++ src/server.js
@@ -13,6 +13,7 @@ app.get('/', (req, res) => res.send('API Running'))
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
 
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
