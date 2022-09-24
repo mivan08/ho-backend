eef71d3b0d9cc7d55d6060ea661fcc42e4896e15
mivan08 <mykhailoivanov662@gmail.com>
2022-09-24 14:01:55 +0300

    Fixing the CORS Policy problem

--- src/server.js
+++ src/server.js
@@ -1,21 +1,23 @@
 const express = require('express')
 const connectDB = require('../config/db')
+const cors = require('cors')
 
 const app = express()
 
 // Connect Database
 connectDB()
 
+// Cors
+const whitelist = ['http://localhost:8888', 'https://geluhorotan.com/']
+var corsOptions = {
+  origin: whitelist,
+  optionsSuccessStatus: 200 // For legacy browser support
+}
+
+app.use(cors(corsOptions))
+
 // Init Middleware
 app.use(express.json({ extended: false }))
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'localhost:3001/') // update to match the domain you will make the request from
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  next()
-})
 
 app.get('/', (req, res) => res.send('API Running'))
 
