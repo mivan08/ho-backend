9e12f1cb19f4cb2181da19e6450be9431bbae73b
Gelu Horotan <100289634+GeluHorotan@users.noreply.github.com>
2023-02-03 00:38:21 +0200

    refactor: changing the controller of project

--- src/routes/api/projects.js
+++ src/routes/api/projects.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { check, validationResult } = require('express-validator')
 const auth = require('../../middleware/auth')
-
+const cloudinary = require('cloudinary').v2
 const Project = require('../../models/Project')
 const User = require('../../models/User')
 
@@ -22,6 +22,61 @@ router.post(
 
     try {
       const user = await User.findById(req.user.id).select('-password')
+      let mobileImage, headerImage, galleryImages
+
+      await cloudinary.uploader.upload(
+        req.body.images.mobile,
+        {
+          resource_type: 'image',
+          folder: `Projects/${req.body.fullProjectName}/mobile`,
+          public_id: 'mobileImage'
+        },
+        (err, result) => {
+          if (err) {
+            console.log(err)
+            return
+          }
+          mobileImage = result.secure_url
+        }
+      )
+      await cloudinary.uploader.upload(
+        req.body.images.header,
+        {
+          resource_type: 'image',
+          folder: `Projects/${req.body.fullProjectName}/header`,
+          public_id: 'headerImage'
+        },
+        (err, result) => {
+          if (err) {
+            console.log(err)
+            return
+          }
+          headerImage = result.secure_url
+        }
+      )
+      req.body.images.gallery.forEach(async image => {
+        await cloudinary.uploader.upload(
+          image,
+          {
+            resource_type: 'image',
+            folder: `Projects/${req.body.fullProjectName}/gallery`,
+            public_id: `galleryImage${Date.now()}`
+          },
+          (err, result) => {
+            if (err) {
+              console.log(err)
+              return
+            }
+            galleryImages.push(result.secure_url)
+          }
+        )
+      })
+
+      let projectImages = {
+        mobile: mobileImage,
+        header: headerImage,
+        gallery: galleryImages
+      }
 
       const newProject = new Project({
         fullProjectName: req.body.fullProjectName,
@@ -32,7 +87,7 @@ router.post(
         abbreviation: req.body.abbreviation,
         team: req.body.team,
         technologies: req.body.technologies,
-        images: req.body.images
+        images: projectImages
       })
 
       const post = await newProject.save()
