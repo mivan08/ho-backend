2de2fd65d8f83b77d881daa6467e4362767ef42d
saboumariusbogdan <100289634+GeluHorotan@users.noreply.github.com>
2023-06-23 03:51:09 +0300

    test: adding timer to db connection for testing purposes 02

--- config/db.js
+++ config/db.js
@@ -4,18 +4,20 @@ const config = require('config')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+// TEST
 const connectDB = async () => {
   const startTime = new Date()
   try {
-    await mongoose.connect(mongoURI, {
+    await mongoose.connect(process.env.MONGOURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      poolSize: poolSize
+      maxPoolSize: 5
     })
     const endTime = new Date()
     console.log(`MongoDB Connected in ${endTime - startTime}ms`)
   } catch (err) {
-    console.error(err.message)
+    console.error(err)
+    // Exit process with failure
     process.exit(1)
   }
 }
